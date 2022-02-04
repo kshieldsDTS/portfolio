04028948eecc2bb05d1d468b22fc7c16521b0d68
kshieldsDTS <escanor05@gmail.com>
2022-02-03 22:06:37 -0500

    complete unstyled hamburger menu on Nav.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Nav from './Components/Nav'
 import Welcome from './Components/Welcome'
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 function Nav(props) {
+    const [openMenu, setOpenMenu] = useState(false)
+    function hamburgerClick(){
+        if (openMenu) {
+            setOpenMenu(false)
+        } else {
+            setOpenMenu(true)
+        }
+    }
     return (
         <div className="nav-wrapper">
-            <Link to='/'>Home</Link>
-            <Link to='/about'>About</Link>
-            <Link to='/projects'>Projects</Link>
-            <Link to='/contact'>Contact</Link>
+            <div className="header-wrapper">
+                <span className="name-span">Kurt Shields</span> 
+                <span className="title-span">Software Engineer</span>   
+            </div>
+            {!openMenu ? <button onClick={hamburgerClick}>Hamburger Menu</button> :
+            <div className="links-wrapper">
+                <Link to='/'>Home</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/projects'>Projects</Link>
+                <Link to='/contact'>Contact</Link>
+                <button onClick={hamburgerClick}>X</button>    
+            </div>}
+            
         </div>
     );
 }
