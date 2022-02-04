4ffd921618dacde83c49b6e236cc6cf1c5a1e123
kshieldsDTS <escanor05@gmail.com>
2022-02-03 21:44:27 -0500

    add NotFound.js and update Contacts to Contact. complete all links and routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-import { Routes, Route, Link, Redirect } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Nav from './Components/Nav'
 import Welcome from './Components/Welcome'
+import About from './Components/About'
+import Projects from './Components/Projects'
+import Contact from './Components/Contact'
+import NotFound from './Components/NotFound'
 
 function App() {
   return (
     <div className="App">
       <Nav/>
-        <Routes>
-          <Route path='/' element={<Welcome />}/>
-            
-        </Routes>
+      <Routes>
+        <Route path='/' element={<Welcome />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/projects' element={<Projects />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/404' element={<NotFound />}/>
+        <Route path='/*' element={<Navigate to='/404'/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact(props) {
+    return (
+        <div>
+            <h1>Hello from Contact.js!</h1>
+        </div>
+    );
+}
+
+export default Contact;
