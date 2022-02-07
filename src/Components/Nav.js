cdf3ff1e2bd7d846a3cd58250476f6ca95a51dec
kshieldsDTS <escanor05@gmail.com>
2022-02-06 23:53:24 -0500

    complete second half of hamburger button animation. need to look into initial rendering

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -15,18 +15,18 @@ function Nav(props) {
                 <span className="name-span">Kurt Shields</span> 
                 <span className="title-span">Software Engineer</span>   
             </div>
-            {!props.openMenu ? <button className="hamburger" onClick={handleClick}>Hamburger Menu</button> :
+            {/* {!props.openMenu ? <button className="hamburger" onClick={handleClick}>Hamburger Menu</button> :
             <div className="links-wrapper">
                 <Link to='/'>Home</Link>
                 <Link to='/about'>About</Link>
                 <Link to='/projects'>Projects</Link>
                 <Link to='/contact'>Contact</Link>
                 <button className="hamburger" onClick={handleClick}>X</button>    
-            </div>}
+            </div>} */}
             <div className="hb-wrapper" onClick={handleClick}>
-                <div className={!props.openMenu ? 'hb-line' : 'hb-line hb-top-change'}></div>
-                <div className={!props.openMenu ? 'hb-line' : 'hb-line hb-middle-change'}></div>
-                <div className={!props.openMenu ? 'hb-line' : 'hb-line hb-bottom-change'}></div>
+                <div className={!props.openMenu ? 'hb-line hb-top' : 'hb-line hb-top-change'}></div>
+                <div className={!props.openMenu ? 'hb-line hb-middle' : 'hb-line hb-middle-change'}></div>
+                <div className={!props.openMenu ? 'hb-line hb-bottom' : 'hb-line hb-bottom-change'}></div>
             </div>
         </div>
     );
