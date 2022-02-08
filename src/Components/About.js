8154a969e82ec10375e55f43feb56cbee975c1d6
kshieldsDTS <escanor05@gmail.com>
2022-02-07 22:51:26 -0500

    add glow effects to about.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,19 +3,23 @@ import React from 'react';
 function About(props) {
     return (
 			<div className='about-wrapper'>
-				<p>
+                <div className="p-wrapper">
+				    <p className='about-p about-p-one'>
 					Hi! The name's Kurt and I have a passion for creating and designing
-					apps that will live on the internet. My software engineering journey
-					began in 2005, in a cramped, repurposed US Army office in the heart of
-					Seoul, Korea. The first tastes of HTML, CSS, and JavaScript would
-					eventually entice me into pursuing a career in software engineering.
-				</p>
-                <p>
-                    While my life took me on a detour down a different industry for over a
-					decade, I have returned to my first passion. I began by getting caught
-					up with an intense Software Engineering Immersive at General Assembly,
-					and I am looking to start my career in the industry.
+				    apps that will live on the internet. My software engineering journey
+				    began in 2005, in a cramped, repurposed US Army office in the heart of
+				    Seoul, Korea. The first tastes of HTML, CSS, and JavaScript would
+				    eventually entice me into pursuing a career in software engineering.
+				    </p>
+                    <div className='gold-one'></div>
+                </div>
+                <div className='gold-two'></div>
+                <div className='p-wrapper'>
+                    <div className='gold-one'></div>
+                <p className='about-p about-p-two'>
+                    While my life took me on a detour down a different industry for over a decade, I have returned to my first passion. I began by getting caught up with an intense Software Engineering Immersive at General Assembly, and I am looking to start my career in the industry.
                 </p>
+                </div>
                 <div>
                     I've recently been using: 
                     <ul>
