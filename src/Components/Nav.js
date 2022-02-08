ae722c380cc11eb15bec1e1b4bc886d35a8347d0
kshieldsDTS <escanor05@gmail.com>
2022-02-07 21:56:34 -0500

    add logic to close menu upon clicking a link

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -10,13 +10,13 @@ function Nav(props) {
         }
     }
     return (
-        <div className='nav-wrapper'>
-        <div className="horizontal-wrapper">
-            <div className="header-wrapper">
-                <span className="name-span">Kurt Shields</span> 
-                <span className="title-span">Software Engineer</span>   
-            </div>
-            {/* {!props.openMenu ? <button className="hamburger" onClick={handleClick}>Hamburger Menu</button> :
+			<div className='nav-wrapper'>
+				<div className='horizontal-wrapper'>
+					<div className='header-wrapper'>
+						<span className='name-span'>Kurt Shields</span>
+						<span className='title-span'>Software Engineer</span>
+					</div>
+					{/* {!props.openMenu ? <button className="hamburger" onClick={handleClick}>Hamburger Menu</button> :
             <div className="links-wrapper">
                 <Link to='/'>Home</Link>
                 <Link to='/about'>About</Link>
@@ -24,29 +24,64 @@ function Nav(props) {
                 <Link to='/contact'>Contact</Link>
                 <button className="hamburger" onClick={handleClick}>X</button>    
             </div>} */}
-            <div className="hb-wrapper" onClick={handleClick}>
-                <div className={!props.openMenu ? 'hb-line hb-top' : 'hb-line hb-top-change'}></div>
-                <div className={!props.openMenu ? 'hb-line hb-middle' : 'hb-line hb-middle-change'}></div>
-                <div className={!props.openMenu ? 'hb-line hb-bottom' : 'hb-line hb-bottom-change'}></div>
-                
-            </div>
-        </div>
-        <nav className={props.openMenu ? 'menu show-menu' : 'menu closed-menu'}>
-            <ul className='link-list'>
-                <Link to='/about'>
-                    <li className={props.openMenu ? 'navlink link-one' : 'navlink hidden-nav-link-one'}>About Me</li>    
-                </Link>
-                <Link to='/projects'>
-                    <li className={props.openMenu ? 'navlink link-two' : 'navlink hidden-nav-link-two'}>Projects</li>
-                </Link>
-                <Link to='/contact'>
-                    <li className={props.openMenu ? 'navlink link-three' : 'navlink hidden-nav-link-three'}>Contacts</li>
-                </Link>
-             
-            </ul>
-        </nav>
-        </div>
-    );
+					<div className='hb-wrapper' onClick={handleClick}>
+						<div
+							className={
+								!props.openMenu ? 'hb-line hb-top' : 'hb-line hb-top-change'
+							}></div>
+						<div
+							className={
+								!props.openMenu
+									? 'hb-line hb-middle'
+									: 'hb-line hb-middle-change'
+							}></div>
+						<div
+							className={
+								!props.openMenu
+									? 'hb-line hb-bottom'
+									: 'hb-line hb-bottom-change'
+							}></div>
+					</div>
+				</div>
+				<nav className={props.openMenu ? 'menu show-menu' : 'menu closed-menu'}>
+					<ul className='link-list'>
+						<Link to='/about'>
+							<li
+								className={
+									props.openMenu
+										? 'navlink link-one'
+										: 'navlink hidden-nav-link-one'
+								}
+								onClick={handleClick}>
+								About Me
+							</li>
+						</Link>
+						<Link to='/projects'>
+							<li
+								className={
+									props.openMenu
+										? 'navlink link-two'
+										: 'navlink hidden-nav-link-two'
+								}
+                                onClick={handleClick}>
+								Projects
+							</li>
+						</Link>
+						<Link to='/contact'>
+							<li
+								className={
+									props.openMenu
+										? 'navlink link-three'
+										: 'navlink hidden-nav-link-three'
+								}
+                                onClick={handleClick}>
+								Contacts
+							</li>
+						</Link>
+					</ul>
+				</nav>
+			</div>
+		);
 }
 
 export default Nav;
