37cd1a8052fbe5176b4a79b6bf2f4976c5dfe150
kshieldsDTS <escanor05@gmail.com>
2023-05-31 09:53:23 -0400

    replace API call for projects with manually entered information due to hosting issues

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -19,35 +19,48 @@ function Projects(props) {
     }, [])
     return (
 			<div className='projects-wrapper'>
-				{fetchedProjects ? (
-					fetchedProjects.map((project, i) => (
-						<div key={`${project.id}${i}`} className='project-div'>
-							<p>{project.name}</p>
-							<img
-								className='project-preview'
-								src={project.preview_image}
-								alt='Kurt and Tofu'></img>
-							<p>{project.tools}</p>
-							<p>{project.description}</p>
-							<a
-								href={project.github_url}
-								target='_blank'
-								rel='noopener noreferrer'>
-								GitHub Repository
-							</a>
-							<div className='gold-two'></div>
-							<a
-								href={project.deployed_url}
-								target='_blank'
-								rel='noopener noreferrer'>
-								Deployed Application
-							</a>
-							<div className='gold-two'></div>
-						</div>
-					))
-				) : (
-					<h1>Heroku is waking up... please stand by!</h1>
-				)}
+				<p>{project.name}</p>
+				<img
+					className='project-preview'
+					src={project.preview_image}
+					alt='Kurt and Tofu'></img>
+				<p>{project.tools}</p>
+				<p>{project.description}</p>
+				<a href={project.github_url} target='_blank' rel='noopener noreferrer'>
+					GitHub Repository
+				</a>
+				<div className='gold-two'></div>
+				<a
+					href={project.deployed_url}
+					target='_blank'
+					rel='noopener noreferrer'>
+					Deployed Application
+				</a>
+				<div className='gold-two'></div>
+				<p>Towers of Hanoi</p>
+				<img
+					className='project-preview'
+					src='https://github.com/kshieldsDTS/towers-of-hanoi/blob/main/assets/towers-of-hanoi.png'
+					alt='Kurt and Tofu'></img>
+				<p>JavaScript, HTML, CSS</p>
+				<p>
+					A simple in-browser recreation of the classic towers of Hanoi game.
+					Complete with logic, win conditions, and prevents illegal moves.
+				</p>
+				<a
+					href='https://github.com/kshieldsDTS/towers-of-hanoi'
+					target='_blank'
+					rel='noopener noreferrer'>
+					GitHub Repository
+				</a>
+				<div className='gold-two'></div>
+				<a
+					href='https://kshieldsdts.github.io/towers-of-hanoi/'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Deployed Application
+				</a>
+				<div className='gold-two'></div>
 			</div>
 		);
 }
